0c65fcc41b743ca6564bc8ca22b35a6090c1e2d8
Rakibul Hasan <rakibulhasan105105@gmail.com>
2026-09-07 02:46:27 +0600

    here we go...

---
+++ A_Raise_Both_Hands.cpp
@@ -0,0 +1,32 @@
+/*  আমার কাছে তোমার লেখা কোন চিঠি নেই, কোন প্রতিস্রতি নেই..
+ *  সাদায় কিন্তু অদ্ভুত তুমি..
+ */
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define fastio  cin.tie(0); ios_base ::sync_with_stdio(0);
+#define ll long long
+
+void phobia(int t)
+{
+    ll l,r;
+    cin >> l >> r;
+    if(l == 1 && r==0) cout << "Yes" << endl;
+    else if(l == 0 && r ==1) cout << "No" << endl;
+    else cout << "Invalid" << endl;
+  return;
+
+}
+ 
+int32_t main()
+{
+    fastio;
+    int tc = 1;
+    //cin >> tc;
+    while (tc--)
+    {
+        phobia(tc);
+    }
+ 
+    return 0; //                Hey, it's like a phobia..
+}
